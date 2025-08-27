1c6095bc164bab2f65e00539141ad39502e7db6f
romainwsite-create <contact@romainwsite.com>
2025-08-27 09:37:27 +0200

    Initial commit

---
+++ src/data/site.ts
@@ -0,0 +1,8 @@
+// src/data/site.ts
+export const SITE_NAME = "Itrora Parfums";
+
+export const WHATSAPP_PHONE = "33650959363";
+export const WHATSAPP_MSG =
+  "Bonjour, je souhaite en savoir plus sur la nouvelle collection Itrora Parfums.";
+export const WHATSAPP_URL =
+  `https://wa.me/${WHATSAPP_PHONE}?text=${encodeURIComponent(WHATSAPP_MSG)}`;
